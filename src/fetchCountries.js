70273f2dfd06e6390933e8342f1c94202154960c
onsnov <114084570+onsnov@users.noreply.github.com>
2023-05-17 17:52:13 +0300

    додає пошук

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,10 +2,10 @@ const URL = "https://restcountries.com/v3.1/all";
 
 
 function fetchCountries(name){ 
-    // return 
-    fetch(
-      `${URL}?aq=${name}&searchIn=title&pageSize=10`
-    ).then(res => res.json());
+     return 
+    fetch(`${URL}?aq=${name}&searchIn=title&pageSize=10`).then(res =>
+      res.json()
+    );
 }
 // fetchCountries('Spain');
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
-import API from "./fetchCountries"
+import API from "./fetchCountries.js";
 
 const DEBOUNCE_DELAY = 300;
 
 const formSearch = document.querySelector('#search-box');
 // console.log(formSearch);
 
-formSearch.addEventListener('input',debounce((onInput), DEBOUNCE_DELAY))  ;   
+formSearch.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));   
+// formSearch.addEventListener('input', (onInput));   
 
 function onInput(evt) {
     evt.preventDefault();
-    const input = evt.currentTarget;
-    const value = input.value.trim();   
+    // const input = evt.target;
+    const value = formSearch.value.trim();   
+    //  console.log(value);
     API.fetchCountries(value)
     .then((result) => console.log(result));
-    // console.log(value);
-  
+     
 }
 
 
