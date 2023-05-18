2005ed0b76b1ad73bc5add4ee047aa3465d369d8
onsnov <114084570+onsnov@users.noreply.github.com>
2023-05-18 16:17:39 +0300

    додає масив

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,10 +2,10 @@ export function fetchCountries(name) {
 
   const URL = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
 
-  function fetchCountries(name) {
+    
     return fetch(`${URL}?aq=${name}&searchIn=title&pageSize=10`).then(res =>
       res.json()
     );
-  }
+  
   // fetchCountries('Spain');
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
-import  { fetchCountries }  from  './fetchCountries';
+import { fetchCountries } from './fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 
 const DEBOUNCE_DELAY = 300;
 
 const formSearch = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+
 
 
 formSearch.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));   
@@ -12,11 +16,20 @@ formSearch.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 function onInput(evt) {
     evt.preventDefault();
     const value = formSearch.value.trim();     
-    console.log(value);
-    fetchCountries(value).then((result) => console.log(result));
-    
-
-     
+    if (!value)  Notify.success('Sol lucet omnibus') ;
+    // console.log(value);
+    fetchCountries(value)
+        .then((res) =>
+            console.log(value));
+         
 }
+
+function onClearInpun(){ 
+    countryList.innerHTML = '';
+}
+
+
+
+
 
 
